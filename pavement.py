1b35ff83db0c03b12c2d6d54d0f342083030ab0d
Barak Korren <barak.korren@gmail.com>
2015-01-06 10:36:20 +0200

    Added downloading and extaracting of artifects
    - Refactored constants into classes
    - Added extracting of BtSync binary archive
    - Added downloading and extracting of btsync-deb sources

--- pavement.py
+++ pavement.py
@@ -6,26 +6,53 @@ from paver.easy import path, task, needs, sh, pushd
 BUILD_DIR=path('build')
 WORK_DIR=BUILD_DIR / 'tmp'
 TARGET_DIR=BUILD_DIR / 'AppDir'
+DIR_MODE=0755
 
-BTSYNC_VERSION='1.4.103'
-BTSYNC_PLATFORM='x64'
-BTSYNC_FILE_PATTERN='btsync_%(p)s-%(v)s.tar.gz'
-BTSYNC_FILE=BTSYNC_FILE_PATTERN % { 'v': BTSYNC_VERSION, 'p': BTSYNC_PLATFORM }
-BTSYNC_URL_PATTERN='http://syncapp.bittorrent.com/%(v)s/%(f)s'
-BTSYNC_URL=BTSYNC_URL_PATTERN % { 'v': BTSYNC_VERSION, 'p': BTSYNC_PLATFORM, 
-        'f': BTSYNC_FILE }
+class BtSync:
+    def __init__(self, version='1.4.103', platform='x86_64', workdir=WORK_DIR):
+        self.version = version
+        self.platform = platform
+        self.platform_nic = { 'x86_64': 'x64' }.get(self.platform, self.platform)
+        self.file_base_name = 'btsync_%s-%s' % (self.platform_nic, self.version)
+        self.archive_name = self.file_base_name + '.tar.gz'
+        self.archive_path = workdir / self.archive_name
+        self.url ='http://syncapp.bittorrent.com/%s/%s' % (self.version,
+                self.archive_name)
+        self.extract_path = workdir / self.file_base_name
 
+class BtSyncDeb:
+    def __init__(self, tag='btsync-1.4.1-1', workdir=WORK_DIR):
+        self.tag = tag
+        self.file_base_name = 'btsync-deb-' + self.tag
+        self.archive_name = self.file_base_name + '.zip'
+        self.archive_path = workdir / self.archive_name
+        self.url = 'https://codeload.github.com/tuxpoldo/btsync-deb/tar.gz/%s' % (
+                self.tag)
+        self.extract_path = workdir / self.file_base_name
 
 @task
 def mk_build_dir():
     """Create directories to build the appdir in"""
-    for dir in (BUILD_DIR, WORK_DIR, TARGET_DIR):
-        path.mkdir(dir, 0o755)
+    for d in (BUILD_DIR, WORK_DIR, TARGET_DIR):
+        d.mkdir(DIR_MODE)
 
 @task
 @needs('mk_build_dir')
 def get_btsync_bin():
     """Download the BtSync binary"""
-    with pushd(WORK_DIR):
-        sh('curl -f %(u)s -o %(f)s' % {'u':BTSYNC_URL, 'f':BTSYNC_FILE})
+    btsync = BtSync()
+    sh('curl -f %s -o %s' % (btsync.url, btsync.archive_path))
+    btsync.extract_path.mkdir(DIR_MODE)
+    sh('tar -xvzf %s -C %s' % (btsync.archive_path, btsync.extract_path))
+
+@task
+@needs('mk_build_dir')
+def get_btsync_deb_src():
+    """Checkout btsync-deb source from Github"""
+    btsd=BtSyncDeb()
+    sh('curl -f %s -o %s' % (btsd.url, btsd.archive_path))
+    btsd.extract_path.mkdir(DIR_MODE)
+    sh('tar -xvzf %s -C %s' % (btsd.archive_path, btsd.extract_path))
+
+
 
