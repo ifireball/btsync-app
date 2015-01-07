fc1f708e0604f3512e07cdf55585b119ae5c56a0
Barak Korren <barak.korren@gmail.com>
2015-01-07 09:40:40 +0200

    Added task to builde btsync-gui locales

--- pavement.py
+++ pavement.py
@@ -29,6 +29,7 @@ class BtSyncDeb:
         self.url = 'https://codeload.github.com/tuxpoldo/btsync-deb/tar.gz/%s' % (
                 self.tag)
         self.extract_path = workdir / self.file_base_name
+        self.gui_path = self.extract_path / 'btsync-gui'
 
 @task
 def mk_build_dir():
@@ -51,8 +52,20 @@ def get_btsync_deb_src():
     """Checkout btsync-deb source from Github"""
     btsd=BtSyncDeb()
     sh('curl -f %s -o %s' % (btsd.url, btsd.archive_path))
-    btsd.extract_path.mkdir(DIR_MODE)
-    sh('tar -xvzf %s -C %s' % (btsd.archive_path, btsd.extract_path))
+    sh('tar -xvzf %s -C %s' % (btsd.archive_path, WORK_DIR))
 
+@task
+@needs('get_btsync_deb_src')
+def build_btsync_gui_locales():
+    """Build the BySyunc GUI locale files"""
+    btsd=BtSyncDeb()
+    locdir = btsd.gui_path / 'locale'
+    locdir.mkdir(DIR_MODE)
+    for pofile in (btsd.gui_path / 'po').glob('*.po'):
+        dstdir = locdir
+        for sd in (pofile.namebase, 'LC_MESSAGES'):
+            dstdir = dstdir / sd
+            dstdir.mkdir(DIR_MODE)
+        sh('msgfmt -c %s -o %s' % (pofile, dstdir / 'btsync-gu.mo'))
 
 
