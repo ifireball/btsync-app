644bb0cee017ca59a2bfd38a311fe44b181bc150
Barak Korren <barak.korren@gmail.com>
2015-01-19 07:37:34 +0200

    Refactored package management code to a class
    Now it will be easier to handle more packages

--- pavement.py
+++ pavement.py
@@ -10,14 +10,14 @@ TARGET_DIR=BUILD_DIR / 'AppDir'
 DIR_MODE=0755
 EXE_MODE=0755
 
-class Package:
+class Package(object):
     """Class for handling software packages"""
 
-    def __init__(self, version, name=None, 
+    def __init__(self, version, workdir, name=None, 
             remote_name=None, remote_prefix=None, 
             local_name=None, local_prefix=None, 
             url=None, urlbase=None,
-            workdir=WORK_DIR, dir_in_archive=True, dirmode=DIR_MODE):
+            dir_in_archive=True, dirmode=DIR_MODE):
         assert url or urlbase
         self.remote_name = (remote_prefix and remote_prefix + '-' or '') + \
                 (remote_name or name or self.name_from_class()) + '-' + version
@@ -26,55 +26,68 @@ class Package:
         self.remote_archive_name = self.remote_name + '.tar.gz'
         self.local_archive_name = self.local_name + '.tar.gz'
         self.url = url or (urlbase + '/' + self.remote_archive_name)
-        self.local_archive_path = wordir / self.local_archive_name
+        self.local_archive_path = workdir / self.local_archive_name
         self.extract_path = workdir / self.local_name
+        self._strip_components = dir_in_archive and 1 or 0
         self._dirmode = dirmode
-        self._dir_in_archive = dir_in_archive
+
+    def download(self):
+        """Download the package"""
+        sh("curl -f '%s' -o '%s'" % (self.url, self.local_archive_path))
+
+    def extract(self):
+        """Extract the packge"""
+        self.extract_path.mkdir(self._dirmode)
+        sh("tar -xvzf '%s' -C '%s' --strip-components=%d" % (\
+                self.local_archive_path, self.extract_path,
+                self._strip_components))
 
     def download_and_extract(self):
         """Download and extract the package"""
-        sh("curl -f '%s' -o '%s'" % (self.url, self.local_archive_path))
-        if self._dir_in_archive:
-            btsync.extract_path.mkdir(self._dirmode)
-        sh("tar -xvzf '%s' -C '%s'" % (btsync.local_archive_path, btsync.extract_path))
+        self.download()
+        self.extract()
 
+    def name_from_class(self):
+        """Convert class name into a package name.
+        Conversion is done by lowercasing all caps and adding dashes in front
+        of them when they follow lowercase letters.
+        For example:
 
-class BtSync:
+        BTSyncDeb => btsync-deb
+        """
+        return re.sub('([a-z]?)([A-Z])',
+                lambda m: (m.group(1) and m.group(1) + '-') + \
+                        m.group(2).lower(), 
+                self.__class__.__name__)
+
+class BTSync(Package):
     def __init__(self, version='1.4.103', platform='x86_64', workdir=WORK_DIR,
-            targetdir=TARGET_DIR):
+            targetdir=TARGET_DIR, dirmode=DIR_MODE):
         self.platform = platform
         self.platform_nic = { 'x86_64': 'x64' }.get(self.platform, self.platform)
-        self.file_base_name = 'btsync_%s-%s' % (self.platform_nic, version)
-        self.archive_name = self.file_base_name + '.tar.gz'
-        self.archive_path = workdir / self.archive_name
-        self.url ='http://syncapp.bittorrent.com/%s/%s' % (version,
-                self.archive_name)
-        self.extract_path = workdir / self.file_base_name
+        super(BTSync, self).__init__(version=version,
+                name='btsync_' + self.platform_nic,
+                urlbase='http://syncapp.bittorrent.com/' + version,
+                dir_in_archive=False, workdir=workdir, dirmode=dirmode)
         self.binary_src_path = self.extract_path / 'btsync'
         self.binary_target_dir = targetdir / 'usr' / 'lib' / 'btsync-common'
         self.binary_target_path = self.binary_target_dir / 'btsync-core'
         self.key_target_dir = targetdir / 'usr' / 'lib' / 'btsync-gui'
         self.key_target_path = self.key_target_dir / 'btsync-gui.key'
 
-class BtSyncDeb:
+class BTSyncDeb(Package):
     def __init__(self, tag='btsync-1.4.1-1', workdir=WORK_DIR):
-        self.file_base_name = 'btsync-deb-' + tag
-        self.archive_name = self.file_base_name + '.zip'
-        self.archive_path = workdir / self.archive_name
-        self.url = \
-            'https://codeload.github.com/tuxpoldo/btsync-deb/tar.gz/' + tag
-        self.extract_path = workdir / self.file_base_name
+        super(BTSyncDeb, self).__init__(version=tag, 
+                url='https://codeload.github.com/tuxpoldo/btsync-deb/tar.gz/' +
+                tag, workdir=workdir)
         self.gui_path = self.extract_path / 'btsync-gui'
         self.install_file = self.gui_path / 'debian' / 'btsync-gui-gtk.install'
 
-class python_qrencode:
+class PythonQREncode(Package):
     def __init__(self, version='1.01', workdir=WORK_DIR, tagretdir=TARGET_DIR):
-        self.file_base_name = 'qrencode-' + version
-        self.archive_name = self.file_base_name + '.tar.gz'
-        self.archive_path = workdir / self.archive_name
-        self.url = 'https://pypi.python.org/packages/source/q/qrencode' + \
-                self.archive_name
-        self.extract_path = workdir / self.file_base_name
+        super(PythonQREncode).__init__(version=version, remote_name='qrencode',
+                urlbase='https://pypi.python.org/packages/source/q/qrencode',
+                workdir=workdir)
 
 @task
 def mk_build_dir():
@@ -86,24 +99,19 @@ def mk_build_dir():
 @needs('mk_build_dir')
 def get_btsync_bin():
     """Download the BtSync binary"""
-    btsync = BtSync()
-    sh("curl -f '%s' -o '%s'" % (btsync.url, btsync.archive_path))
-    btsync.extract_path.mkdir(DIR_MODE)
-    sh("tar -xvzf '%s' -C '%s'" % (btsync.archive_path, btsync.extract_path))
+    BTSync().download_and_extract()
 
 @task
 @needs('mk_build_dir')
 def get_btsync_deb_src():
     """Checkout btsync-deb source from Github"""
-    btsd=BtSyncDeb()
-    sh("curl -f '%s' -o '%s'" % (btsd.url, btsd.archive_path))
-    sh("tar -xvzf '%s' -C '%s'" % (btsd.archive_path, WORK_DIR))
+    BTSyncDeb().download_and_extract()
 
 @task
 @needs('get_btsync_deb_src')
 def build_btsync_gui_locales():
     """Build the BySync GUI locale files"""
-    btsd=BtSyncDeb()
+    btsd=BTSyncDeb()
     locdir = btsd.gui_path / 'locale'
     locdir.mkdir(DIR_MODE)
     for pofile in (btsd.gui_path / 'po').glob('*.po'):
@@ -116,8 +124,8 @@ def build_btsync_gui_locales():
 @task
 @needs('build_btsync_gui_locales')
 def install_btsync_gui():
-    """Install the BtSync GUI files to the AppDir"""
-    btsd=BtSyncDeb()
+    """Install the BTSync GUI files to the AppDir"""
+    btsd=BTSyncDeb()
     with open(btsd.install_file, 'r') as f:
         for l in f:
             if re.match('^\s*(#|$)', l):
@@ -131,8 +139,8 @@ def install_btsync_gui():
 @task
 @needs('get_btsync_bin')
 def install_btsync_bin():
-    """Install the BtSync binary to the AppDir"""
-    btsync = BtSync()
+    """Install the BTSync binary to the AppDir"""
+    btsync = BTSync()
     btsync.binary_target_dir.makedirs(DIR_MODE)
     sh("install -m %o '%s' '%s'" % (EXE_MODE, btsync.binary_src_path,
         btsync.binary_target_path))
@@ -141,7 +149,7 @@ def install_btsync_bin():
 @needs('mk_build_dir')
 def install_api_token():
     """Install the API token to the AppDir"""
-    btsync = BtSync()
+    btsync = BTSync()
     btsync.key_target_dir.makedirs(DIR_MODE)
     path('btsync-api-token.txt').copyfile(btsync.key_target_path)
 
